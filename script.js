dd030877f5c5691d3415ddd27718a8620390bf1c
ZidanSenpai <crazyzdan786@gmail.com>
2025-06-22 02:31:57 +0530

    add a play round function that plays a round of the game

--- script.js
+++ script.js
@@ -2,30 +2,53 @@ function getComputerChoice() {
     let x = Math.random();
     let choice = "";
     if (x<= 0.33) {
-        choice = "Rock";
+        choice = 1;
     }
     else if (x<=0.66 && x> 0.33 ) {
-        choice = "Paper";
+        choice = 2;
     }
     else if (x<=0.99 && x > 0.66){
-        choice = "Scissor";
+        choice = 3;
     }
     return choice;
 }
 function getHumanChoice() {
-    let choice = "";
-    let x = prompt("Enter your choice: 1- rock 2- paper 3- Scissor");
-    if (x == 1) {
-        return "Rock";
+    let x = prompt("Enter your choice: rock, paper, scissor");
+    let y = x.toLowerCase();
+    if (y == "rock") {
+        return 1;
     }
-    else if(x==2){
-        return "Paper";
+    else if(y=="paper"){
+        return 2;
     }
-    else if(x==3){
-        return "Scissor";
+    else if(y=="scissor"){
+        return 3;
     }
-    else return "Invalid option";
+    else return 4;
 }
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
-console.log(humanChoice);
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == 1 && computerChoice == 2) {
+        console.log("Rock was beaten by Paper, You Lose!");
+    }
+    else if(humanChoice == 1 && computerChoice == 3){
+        console.log("Rock beats Paper, You Win!");
+    }
+    else if(humanChoice == computerChoice){
+        console.log("You both chose the same, It's a tie");
+    }
+    else if(humanChoice == 2 && computerChoice == 1){
+        console.log("Paper beats Rock, You Win!");
+    }
+    else if(humanChoice == 2 && computerChoice == 3){
+        console.log("Paper was beaten by Scissors, You Lose!");
+    }
+    else if(humanChoice == 3 && computerChoice == 1){
+        console.log("Scissor was beaten by Rock, You Lose!");
+    }
+    else if(humanChoice == 3 && computerChoice == 2){
+        console.log("Scissor beat Paper, You win!");
+    }
+}
+playRound(getHumanChoice(), getComputerChoice());
